aad8ca65e0f5d253435bf6cb2ef23ccec184f7ac
Ross <mrvr@rams.colostate.edu>
2021-02-26 13:08:26 -0700

    Updating book_pusher

--- utilities/book_pusher.R
+++ utilities/book_pusher.R
@@ -1,7 +1,9 @@
 #This script moves the book generated here to my personal website
 # repository. 
 
+## Run this line
 bookdown::render_book('01_WR418.Rmd','all')
+
 # Mostly copied from this nice blog post: 
 #https://amywhiteheadresearch.wordpress.com/2014/11/12/copying-files-with-r/
 #get where book lives
